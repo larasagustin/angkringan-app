f676eb77290417ae4cedcf0f6ccd715704cf6d83
larasagustin <150769974+larasagustin@users.noreply.github.com>
2025-10-29 19:12:29 +0700

    membuat view pada kelola menu

---
+++ resources/views/Admin/Menus/index.blade.php
@@ -0,0 +1,10 @@
+@extends('Admin.layouts.app')
+
+@section('title', 'Kelola Menu')
+
+@section('content')
+<div class="container py-4">
+    <h1 class="text-2xl font-bold mb-4">Kelola Menu</h1>
+    <p>Halaman ini akan menampilkan daftar menu.</p>
+</div>
+@endsection
